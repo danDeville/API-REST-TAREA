9ef5e66682159948fa91d1027fe67a790a7daba4
geral-09 <geral.martinez.0919@gmail.com>
2022-03-30 18:22:27 -0500

    se agrega un script.js con información de los participantes

---
+++ scripts/integrantes.js
@@ -0,0 +1,56 @@
+const integrantes = document.getElementById("footer")
+
+function int (){
+    let contenedor = document.getElementById("footer")
+    contenedor.innerHTML =  `<div class="card" style="width: 18rem;">
+    <img src="https://res.cloudinary.com/dnb1vdvcn/image/upload/v1648681722/c93f5023-d6ea-4472-8919-0b0468d75fc4_fkhkkn.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <ul>
+      <h5 class="card-title">Geraldine</h5><br>
+      <li><h5 class="card-title">Edad: 17</h5></li>
+      <li><h5 class="card-title">De: Medellín</h5></li>
+      <li><p class="card-text">Técnica auxiliar en desarrolo de software con muy buena lógica de programación, conocimientos básicos en algunos lenguajes de  programación como (Java, Phyton, Visual Basic, entre otros), en este momento me encuentro encaminada a Front end (HTML, CSS, JavaScript).</p></li>
+      </ul>
+    </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+    <img src="https://res.cloudinary.com/dz8on44po/image/upload/v1648507049/Foto_de_Daniel_Tinjaca_sxnofo.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <ul>
+      <h5 class="card-title">Daniel Tinjaca</h5><br>
+      <li><h5 class="card-title">Edad: 27</h5></li>
+      <li><h5 class="card-title">De: Bogotá</h5></li>
+      <li><p class="card-text">Desarrollador Frontend en formación con más de dos años de experiencia en Maquetación Web. Entusiasta en aprender tecnologías encaminadas al Desarrollo Web',
+      lenguajes: 'HTML & CSS'</p></li>
+      </ul>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img src="https://res.cloudinary.com/dnb1vdvcn/image/upload/v1648681680/82c61acf-aa45-4521-b048-994a4685d80c_krnrjr.jpg" alt="...">
+    <div class="card-body">
+      <ul>
+      <h5 class="card-title">Jackelin Rodriguez</h5><br>
+      <li><h5 class="card-title">Edad: 20</h5></li>
+      <li><h5 class="card-title">De: Bogotá</h5></li>
+      <li><p class="card-text">No poseo experiencia, pero tengo toda la actitud para aprender.</p></li>
+      </ul>
+    </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+    <img src="https://res.cloudinary.com/dp9zv16le/image/upload/v1648506286/foto_y11noe.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <ul>
+      <h5 class="card-title">Dilan Espindola</h5><br>
+      <li><h5 class="card-title">Edad: 20</h5></li>
+      <li><h5 class="card-title">De: Bogotá</h5></li>
+      <li><p class="card-text">conocimientos: javascript, html, css, un poco de react, nodejs</p></li>
+      </ul>
+    </div>
+    </div>
+    `
+}
+
+int ();
